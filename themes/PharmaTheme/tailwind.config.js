f15900156f2a0b1573f1c18409ba5fe57948879b
Az3r1y <dario.beissmann@icloud.com>
2024-06-18 19:44:09 +0200

    Initial commit

---
+++ themes/PharmaTheme/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './hugo_stats.json',
+    './layouts/**/*.html',
+    './content/**/*.md',
+    './static/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
